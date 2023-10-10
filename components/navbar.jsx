2f6951bfa6f81725157b358752247938c21cdfa5
NotMatter60 <matthias.wuyts60@gmail.com>
2023-10-10 22:54:43 +0200

    logo groter gmkt

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,59 +1,54 @@
-"use client";
-
+"use client"
 import Link from "next/link";
-
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-    navigationMenuTriggerStyle,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
 function NavBar() {
-    return (
-        <div className="hidden lg:block supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur dark:border-zinc-800">
-            <div className="container flex h-14 center-items">
-                    <div className="mr-4 hidden md:flex">
-                        <img
-                            src="/logo.svg" 
-                            width={100}
-                            height={100}
-                            href="/"
-                        />
-                    </div>
-                    <NavigationMenu>
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <Link href="/docs" passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        Home
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <Link href="/contact" passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        New Releases
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <Link href="/contact" passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        Top
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-            </div>
+  return (
+    <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur dark:border-zinc-800">
+      <div className="container flex items-center">
+        <div className="mr-4">
+          <img
+            src="/logo.svg"
+            className="w-24 h-16"  // Aangepaste breedte, hoogte behouden
+            alt="Logo"
+            href="/"
+          />
         </div>
-    )
+        <NavigationMenu>
+          <NavigationMenuList className="flex flex-col md:flex-row md:ml-4">
+            <NavigationMenuItem>
+              <Link href="/docs" passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contact" passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  New Releases
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contact" passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Top
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
