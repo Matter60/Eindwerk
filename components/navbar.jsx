e0af8738c6ad1422223a348e81aabad44e311ebb
Matter <matthias.wuyts60@gmail.com>
2023-10-10 14:16:20 +0200

    logo toegevoegd

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 
@@ -12,39 +12,47 @@ import {
     NavigationMenuTrigger,
     NavigationMenuViewport,
     navigationMenuTriggerStyle,
-  } from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
 
+function NavBar() {
+    return (
+        <div className="hidden lg:block supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur dark:border-zinc-800">
+            <div className="container flex h-14 center-items">
+                    <div className="mr-4 hidden md:flex">
+                        <img
+                            src="/logo.svg" 
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <NavigationMenu>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <Link href="/docs" passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Home
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <Link href="/contact" passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        New Releases
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <Link href="/contact" passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Top
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+            </div>
+        </div>
+    );
+}
 
-  function NavBar() {
-    return(
-        <center>
-        <NavigationMenu>
-  <NavigationMenuList>
-  <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Home
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/contact" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              New Releases
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/contact" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Top
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-
-    </center>
-    )
-  }
-  export default NavBar
+export default NavBar;
