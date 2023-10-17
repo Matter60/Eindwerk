7e0c542bc49a157a6219a4e040f9ddfd159f2b55
NotMatter60 <matthias.wuyts60@gmail.com>
2023-10-17 22:00:06 +0200

    fixed genre list

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import GenreList from "@/components/GenreList";
-
 export default function Home() {
   return (
     <>
-      <GenreList />
+      <div className="grid grid-cols-4 px-8">
+        <div className="hidden md:block">
+          <GenreList />
+        </div>
+        <div className="col-span4 md:col-span-3"></div>
+      </div>
     </>
   );
 }
--- components/GenreList.jsx
+++ components/GenreList.jsx
@@ -1,4 +1,5 @@
 export async function getGenres() {
+  
   const response = await fetch(
     `https://api.rawg.io/api/genres?key=${process.env.RAWG_API_KEY}`
   );
@@ -16,10 +17,11 @@ export async function GenreList() {
   console.log(genres);
   return (
     <div>
+      <h2 className="text-[30px] font-bold">Genre</h2>
       {genres.map((genre) => (
-        <div className="flex gap-2 items-center"key={genre.id}>
-          <img className="w-[40px] h-[25px]" src={genre.image_background} alt={genre.name} />
-          <h2>{genre.name}</h2>
+        <div key={genre.id} className="flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
+          <img key={genre.id} className="w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300" src={genre.image_background} alt={genre.name} />
+          <h3 className="group-hover:font-bold group-hover:scale-105 transition-all ease-out duration-300">{genre.name}</h3>
           
         </div>
       ))}
