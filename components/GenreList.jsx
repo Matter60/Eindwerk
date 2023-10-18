d9124556249be95353cfdbe6b4fbb3d8beaa992f
Matter <matthias.wuyts60@gmail.com>
2023-10-18 10:04:46 +0200

    genre-id

--- components/GenreList.jsx
+++ components/GenreList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export async function getGenres() {
   
   const response = await fetch(
@@ -19,11 +21,11 @@ export async function GenreList() {
     <div>
       <h2 className="text-[30px] font-bold">Genre</h2>
       {genres.map((genre) => (
-        <div key={genre.id} className="flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
+        <Link href={`/genre/${genre.id}`} key={genre.id} className="flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg">
           <img key={genre.id} className="w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300" src={genre.image_background} alt={genre.name} />
           <h3 className="group-hover:font-bold group-hover:scale-105 transition-all ease-out duration-300">{genre.name}</h3>
           
-        </div>
+        </Link>
       ))}
     </div>
   );
