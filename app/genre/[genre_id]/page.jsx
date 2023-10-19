e681748e054cf2a53424841cfb8fe247f2ae5a61
Matter <matthias.wuyts60@gmail.com>
2023-10-19 10:01:43 +0200

    GamesByGenre added

--- app/genre/[genre_id]/page.jsx
+++ app/genre/[genre_id]/page.jsx
@@ -4,5 +4,5 @@ import { useParams } from "next/navigation";
 export default function Page() {
   const params = useParams();
   /*{params.genre_id} genre id*/
-  return <div></div>;
+  return <></>;
 }
