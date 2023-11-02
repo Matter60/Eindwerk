db8dfb7a4ee2e9c483c23c8802e8342571a26726
NotMatter60 <matthias.wuyts60@gmail.com>
2023-11-03 00:18:40 +0100

    added search bar not functional

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -11,54 +11,34 @@ import {
 } from "@/components/ui/navigation-menu";
 import { ModeToggle } from "./dark-mode-toggle";
 import Image from "next/image";
+import { Search } from "lucide-react";
+import SearchBar from "./Searchbar";
 
 function NavBar() {
   return (
     <div className="sticky top-0 z-50 w-full border-b bg-background/95 p-2 backdrop-blur dark:border-zinc-800">
       <div className="container flex items-center justify-between">
         <div className="hidden md:flex">
-          <Image
-            src="/logo.svg"
-            className="flex items-center dark:hidden" // Aangepaste breedte, hoogte behouden
-            alt="Logo"
-            href="/"
-            width={120}
-            height={80}
-          />
-          <Image
-            src="/logo-white.svg"
-            className="items-center hidden dark:block" // Aangepaste breedte, hoogte behouden
-            alt="Logo"
-            href="/"
-            width={120}
-            height={80}
-          />
+          <a href="/">
+            <Image
+              src="/logo.svg"
+              className="flex items-center dark:hidden" // Aangepaste breedte, hoogte behouden
+              alt="Logo"
+              href="/"
+              width={120}
+              height={80}
+            />
+            <Image
+              src="/logo-white.svg"
+              className="items-center hidden dark:block" // Aangepaste breedte, hoogte behouden
+              alt="Logo"
+              href="/"
+              width={120}
+              height={80}
+            />
+          </a>
         </div>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Home
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  New releases
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Top
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <SearchBar />
         <ModeToggle />
       </div>
     </div>
