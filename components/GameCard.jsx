576de8e41e918a06b65becac96da34dbcfa8019c
NotMatter60 <matthias.wuyts60@gmail.com>
2023-11-14 22:19:02 +0100

    metacritic

--- components/GameCard.jsx
+++ components/GameCard.jsx
@@ -11,31 +11,32 @@ import {
 const GameCard = ({ game }) => (
   <Link href={`/game/${game.slug}`} key={game.id} className="h-full">
     <Card className="border border-muted h-full hover:bg-primary-foreground hover:scale-105 transition duration-100 hover:shadow-lg">
+      <div className="relative h-[187px] w-full">
+        <img
+          src={game.background_image}
+          alt={game.name}
+          className="rounded object-cover w-full h-full"
+        />
+        {game.metacritic && (
+          <p
+            style={{
+              backgroundColor: "#4CAF50",
+              border: "2px solid #4CAF50",
+              padding: "4px",
+              borderRadius: "4px",
+              position: "absolute",
+              top: "0",
+              right: "0",
+            }}
+          >
+            {game.metacritic}
+          </p>
+        )}
+      </div>
       <CardContent className="p-3">
-        <div className="h-[187px] w-full">
-          <img
-            src={game.background_image}
-            alt={game.name}
-            className="rounded object-cover w-full h-full"
-          />
-        </div>
-
         <div className="my-3">
           <CardTitle>{game.name}</CardTitle>
           <CardDescription>{game.released}</CardDescription>
-          {game.metacritic && (
-            <p
-              style={{
-                backgroundColor: "#4CAF50",
-                border: "2px solid #4CAF50",
-                padding: "4px",
-                borderRadius: "4px",
-                display: "inline-block",
-              }}
-            >
-              {game.metacritic}
-            </p>
-          )}
           {/* Add more details as needed */}
         </div>
       </CardContent>
