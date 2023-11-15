6a04b9c6082090fc5ba9b10869635de8b65609f6
Matter <matthias.wuyts60@gmail.com>
2023-11-15 11:29:13 +0100

    fixed searchbar error v2

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -4,12 +4,6 @@ import { Input } from "@/components/ui/input";
 
 export default function SearchBar() {
   const [query, setQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-
-  const searchGames = () => {
-    const games = getGames(query);
-    setSearchResults(games);
-  };
 
   return (
     <form action="/search" method="get" className="flex items-center space-x-2">
@@ -19,7 +13,7 @@ export default function SearchBar() {
         className="px-3 py-2 w-80"
         placeholder="Search..."
       />
-      <Button type="submit" className="px-3 py-2" onClick={searchGames}>
+      <Button type="submit" className="px-3 py-2">
         Search
       </Button>
     </form>
