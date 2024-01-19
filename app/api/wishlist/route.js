c4e46a68e9e27e8b16ab2adc07adb08fa9254b53
Matter <matthias.wuyts60@gmail.com>
2024-01-19 10:02:01 +0100

    niks gefixed

--- app/api/wishlist/route.js
+++ app/api/wishlist/route.js
@@ -1,12 +1,30 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
 
-export async function POST(req) {
+export async function POST() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  if (!data) return NextResponse.error(400, "No data provided");
+
+  console.log(userId);
+  prisma.whishlist.create({
+    data: {},
+  });
+
+  return NextResponse.json({ data });
+}
+
+/*export async function POST(req) {
   const data = await req.json();
 
   const { sessionId, getToken } = await req.auth;
 
-  console.log(data, sessionId);
+  console.log(data, sessionId, userId);
 
   if (!data) return NextResponse.error(400, "No data provided");
 
@@ -15,4 +33,4 @@ export async function POST(req) {
   });
 
   return NextResponse.json(data);
-}
+}*/
--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
 // Remove authMiddleware from the default export
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware;
 export const config = {
   matcher: [], // Empty array means all routes are public
 };
