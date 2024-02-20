97a4c67fe82f4d7a3407631dfabbb4d591e609a7
Matter <matthias.wuyts60@gmail.com>
2024-02-20 11:10:44 +0100

    fxied middleware

--- middleware.js
+++ middleware.js
@@ -4,7 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    /^\/$/, // Matches "/"
+    /^\/game\/[^\/]+$/, // Matches "/game/[slug]" where [slug] can be any non-empty string
+    /^\/search$/, // Matches "/search"
+    /^\/genre\/[^\/]+$/, // Matches "/genre/[genre_id]" where [genre_id] can be any non-empty string
+  ],
 });
 
 export const config = {
